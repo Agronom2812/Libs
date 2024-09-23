700e3ed70fb32371c7307dd6142fbf804f6a00ed
Yury Zheludovich <160975896+Agronom2812@users.noreply.github.com>
2024-09-23 23:53:08 +0200

    Add files of EmailValidatorJsonLibrary
    This commit adds library files to pre-check them

---
+++ EmailValidatorJson/EmailValidatorJsonCore/EmailValidator.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.RegularExpressions;
+
+namespace EmailValidatorJson.EmailValidatorJsonCore
+{
+    /// <summary>
+    /// A class for validating emails
+    /// </summary>
+    public class EmailValidator
+    {
+        /// <summary>
+        /// A regular expression pattern used for validating email addresses.
+        /// </summary>
+        /// <remarks>
+        /// This pattern checks for a basic structure of an email address, ensuring that:
+        /// 1. There is at least one character before the '@' symbol.
+        /// 2. There is a domain name after the '@' symbol.
+        /// 3. The domain name includes a dot ('.') followed by at least one character.
+        /// </remarks>
+        private string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+        /// <summary>
+        /// Validates the format of the provided email address.
+        /// </summary>
+        /// <param name="email">The email adress to validate</param>
+        /// <returns>
+        /// <c>true</c> if the email address matches the defined pattern; otherwise, <c>false</c>.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">Thrown if the provided email address is null.</exception>
+        public bool Validation(string email)
+        {
+            return Regex.IsMatch(email, pattern);
+        }
+    }
+}
