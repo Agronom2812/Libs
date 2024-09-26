41688fa6f3f4c652f9363df87423ad6b15c570ca
Yury Zheludovich <160975896+Agronom2812@users.noreply.github.com>
2024-09-26 20:08:10 +0200

    Add files for EmailValidatorJsonLib
    FIles contain source code, tests and example file wchich shows how user can use this lib

---
+++ source/EmailValidatorJsonCore/JsonParser.cs
@@ -0,0 +1,61 @@
+﻿using EmailValidatorJson.EmailValidatorJsonCore.Models;
+using Newtonsoft.Json;
+
+namespace EmailValidatorJson.EmailValidatorJsonCore
+{
+    /// <summary>
+    /// A class for extracting unique email addresses from a JSON file
+    /// </summary>
+    public class JsonParser
+    {
+        /// <summary>
+        /// Extracts unique email addresses from a JSON file containing team information.
+        /// </summary>
+        /// <param name="filePath">The path to the JSON file from which to extract email addresses.</param>
+        /// <returns>
+        /// A list of unique email addresses extracted from the JSON data. If an error occurs, 
+        /// an empty list is returned.
+        /// </returns>
+        /// <remarks>
+        /// This method reads the contents of the specified JSON file and deserializes it into a list
+        /// of <see cref="Teams"/> objects. It collects email addresses from both team members and 
+        /// subteam members. If the JSON structure is invalid or an error occurs during file reading,
+        /// the method handles the exception and outputs an error message.
+        /// </remarks>
+        /// <exception cref="FileNotFoundException">Thrown if the specified file does not exist.</exception>
+        /// <exception cref="JsonException">Thrown if the JSON is invalid or cannot be deserialized.</exception>
+        public List<string> ExtractEmails(string filePath)
+        {
+            try
+            {
+                var json = File.ReadAllText(filePath);
+
+                var teams = JsonConvert.DeserializeObject<List<Teams>>(json);
+
+                var emails = new List<string>();
+
+                if (teams != null)
+                {
+                    foreach (var team in teams)
+                    {
+                        emails.AddRange(team.Members);
+
+                        if (team.Subteams != null)
+                        {
+                            foreach (var subteam in team.Subteams)
+                            {
+                                emails.AddRange(subteam.Members);
+                            }
+                        }
+                    }
+                }
+                List<string> uniqueEmails = emails.Distinct().ToList();
+                return uniqueEmails;
+            }
+            catch
+            {
+                throw new JsonReaderException();
+            }
+        }
+    }
+}
---
+++ source/EmailValidatorJsonCore/PrintEmailList.cs
@@ -0,0 +1,122 @@
+﻿namespace EmailValidatorJson.EmailValidatorJsonCore
+{
+    /// <summary>
+    /// A class for outputting a list of emails
+    /// </summary>
+    public class PrintEmailList
+    {
+        /// <summary>
+        /// An instance of the <see cref="EmailValidator"/> class used for validating email addresses.
+        /// </summary>
+        private EmailValidator validator = new EmailValidator();
+
+        /// <summary>
+        /// An instance of the <see cref="Random"/> class used for generating random numbers.
+        /// </summary>
+        private Random rng = new Random();
+
+        /// <summary>
+        /// Sorts a list of email addresses based on their validity and outputs the sorted list.
+        /// </summary>
+        /// <param name="emails">The list of emails to sort</param>
+        /// <remarks>
+        /// The method uses an instance of <see cref="EmailValidator"/> to determine if each email is valid.
+        /// Valid emails are prioritized in the sorted output. Invalid emails are appended with a message
+        /// indicating their invalidity.
+        /// The sorting of valid emails is randomized using an instance of <see cref="Random"/>.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">Thrown if the provided list of email address is null.</exception>
+        public List<string> RandomEmailSort(List<string> emails)
+        {
+            if (emails.Count == 0)
+                return new List<string>();
+
+            List<string> sortedEmails = emails
+            .Select(email => new
+            {
+                Email = email,
+                IsValid = validator.Validation(email)
+            })
+            .OrderBy(x => x.IsValid ? 0 : 1)
+            .ThenBy(x => x.IsValid ? rng.Next() : 0)
+            .Select(x => x.IsValid ? x.Email : x.Email + " - Invalid email")
+            .ToList();
+
+            foreach (var email in sortedEmails)
+            {
+                    Console.WriteLine(email);
+            }
+
+            return sortedEmails;
+        }
+
+        /// <summary>
+        /// Sorts a list of email addresses based on their validity and outputs the sorted list.
+        /// </summary>
+        /// <param name="emails">The list of emails to sort</param>
+        /// <remarks>
+        /// The method uses an instance of <see cref="EmailValidator"/> to determine if each email is valid.
+        /// Valid emails are prioritized in the sorted output. Invalid emails are appended with a message indicating their invalidity.
+        /// Valid emails are sorted in lexicographic order.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">Thrown if the provided list of email address is null.</exception>
+        public List<string> AlphabetEmailSort(List<string> emails)
+        {
+            if (emails.Count == 0)
+                return new List<string>();
+
+            List<string> sortedEmails = emails
+            .Select(email => new
+             {
+                 Email = email,
+                 IsValid = validator.Validation(email)
+             })
+            .OrderBy(x => x.IsValid ? 0 : 1)
+            .ThenBy(x => x.Email)
+            .Select(x => x.IsValid ? x.Email : x.Email + " - Invalid email")
+            .ToList();
+
+            foreach (var email in sortedEmails)
+            {
+                Console.WriteLine(email);
+            }
+
+            return sortedEmails;
+        }
+
+        /// <summary>
+        /// Sorts a list of email addresses based on their validity and outputs the sorted list.
+        /// </summary>
+        /// <param name="emails">The list of emails to sort</param>
+        /// <remarks>
+        /// The method uses an instance of <see cref="EmailValidator"/> to determine if each email is valid.
+        /// Valid emails are prioritized in the sorted output. Invalid emails are appended with a message indicating their invalidity.
+        /// Valid emails are sorted in lexicographic order of their domains.
+        /// </remarks>
+        /// <exception cref="ArgumentNullException">Thrown if the provided list of email address is null.</exception>
+        public List<string> DomainEmailSort(List<string> emails)
+        {
+            if (emails.Count == 0)
+                return new List<string>();
+
+            List<string> sortedEmails = emails
+            .Select(email => new
+            {
+                Email = email,
+                IsValid = validator.Validation(email),
+                Domain = validator.Validation(email) ? email.Substring(email.IndexOf('@') + 1) : null
+            })
+            .OrderBy(x => x.IsValid ? 0 : 1)
+            .ThenBy(x => x.Domain ?? string.Empty)
+            .Select(x => x.IsValid ? x.Email : x.Email + " - Invalid email")
+            .ToList();
+
+            foreach (var email in sortedEmails)
+            {
+                Console.WriteLine(email);
+            }
+
+            return sortedEmails;
+        }
+    }
+}
